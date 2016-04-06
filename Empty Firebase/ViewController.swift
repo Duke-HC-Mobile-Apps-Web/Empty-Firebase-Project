d0e9a38594cb9eebe995385aa552c490af94f907
Davis Gossage <dcg13@duke.edu>
2016-04-06 19:29:39 -0400

    Initial commit

--- Empty Firebase/ViewController.swift
+++ Empty Firebase/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create a reference to a Firebase location
+        let myRootRef = Firebase(url:"https://<YOUR-FIREBASE-APP>.firebaseio.com")
+        // Write data to Firebase
+        myRootRef.setValue("Do you have data? You'll love Firebase.")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
